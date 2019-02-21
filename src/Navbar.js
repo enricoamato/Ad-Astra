5a08101aa53c6de7f20be140e3b56e2a02e405fd
unknown <enricoamato997@gmail.com>
2019-02-21 22:05:27 +0100

    add navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="navbarContainer">
+      <nav>
+        <ul>
+          <a href="#">Home</a>
+          <a href="https://api.nasa.gov/" target="_blank" rel="noopener noreferrer">NASA API</a>
+          <a href="#">About</a>
+          <a href="#">Resume</a>
+        </ul>
+      </nav>
     </div>
   )
 }
