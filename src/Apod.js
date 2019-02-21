c7ae81cb465d6e8f7a425ae7d5208e9badf89c2b
unknown <enricoamato997@gmail.com>
2019-02-21 22:05:59 +0100

    add API loading & conditional rendering

--- src/Apod.js
+++ src/Apod.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 class Apod extends React.Component {
   state = {
+    total: undefined,
     date: undefined,
     explanation: undefined,
-    apodPicUrl: undefined
+    apodPicUrl: undefined,
+    isLoading: true,
+    resParam: undefined
   }
 
   componentDidMount() {
@@ -13,27 +16,35 @@ class Apod extends React.Component {
     fetch(`${endpoint}?api_key=${key}`)
       .then(res => res.json())
       .then(res => this.setState({
+        total: res,
         date: res.date,
         explanation: res.explanation,
-        apodPicUrl: res.hdurl
+        apodPicUrl: res.hdurl,
+        isLoading: false
       }))
   }
 
   render() {
-    return(
-      <div className="apod">
-        <h1>Astronomy Picture of the Day</h1>
-        <h4>{this.state.date}</h4>
-        <a href={this.state.apodPicUrl} target="_blank">
-          <img alt="apodPic" src={this.state.apodPicUrl} />
-        </a>
-        <p>{this.state.explanation}</p>
 
+    if(!this.state.isLoading){
+      return(
+        <div className="apod">
+          <h1>Astronomy Picture of the Day</h1>
+          <h4>{this.state.date}</h4>
+          <a href={this.state.apodPicUrl} target="_blank" rel="noopener noreferrer">
+            <img alt="apodPic" src={this.state.apodPicUrl} />
+          </a>
+          <p>{this.state.explanation}</p>
           <button className="fullscreenPic">VIEW FULL SCREEN PIC</button>
+        </div>
+      )
+    } else {
+      return(
+        <h1>Loading</h1>
+      )
+    }
 
-      </div>
-    )
-  }
+  } //render
 } //Apod class
 
 export default Apod
