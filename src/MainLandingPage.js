eeb970dab49aafa6c15fc815338f799c4bcd68bc
unknown <enricoamato997@gmail.com>
2019-02-21 22:07:08 +0100

    change title

--- src/MainLandingPage.js
+++ src/MainLandingPage.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import './index.css'
 
 const MainLandingPage = () => {
   return (
     <div className="MainLandingPageDiv">
-
+      <h1>Ad Astra</h1>
     </div>
   )
 }
