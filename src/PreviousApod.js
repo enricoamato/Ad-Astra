93a2b1aefa64c0fb8609a212675aec0a0ff936b9
unknown <enricoamato997@gmail.com>
2019-02-26 03:57:28 +0100

    not used at the moment

--- src/PreviousApod.js
+++ src/PreviousApod.js
@@ -1,119 +1,101 @@
 import React from 'react'
 
-const PreviousApod = (props) => {
-
-  function mediaType(){
-    if(props.media_type === 'video'){
-      return <iframe src={props.mediaUrl} frameborder="0" allowfullscreen="true" title="nasa"></iframe>
-    }else{
-      return <img src={props.mediaUrl} alt="Apod"></img>
+class PreviousApod extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      total: undefined,
+      oldDate: props.date,
+      newDate: undefined,
+      explanation: undefined,
+      mediaUrl: undefined,
+      mediaType: undefined,
+      isLoading: true
     }
   }
 
-  return(
-    <div className="gridSystem">
+  mediaType(){
+    if(this.state.mediaType === 'video'){
+      return <iframe src={this.state.mediaUrl} allowFullScreen={true} title="nasa"></iframe>
+    }else{
+      return <img src={this.state.mediaUrl} alt="Apod"></img>
+    }
+  }
+
+  getPreviousDate(date){
+    var splittedDate = date.split("-");
+    var year = splittedDate[0]
+    var month = splittedDate[1]
+    var day = splittedDate[2]
+
+    var thirtyone = [1,3,5,7,8,10,12]
+    var thirty = [4,6,9,11]
+    var twentyeight = [2]
+
+    var typeDay = undefined
+    var newMonth = undefined
+
+    var lastDayOfMonth = {
+      "thirtyone" : "31",
+      "thirty" : "30",
+      "twentyeight" : "28"
+    }
+
+    if(+day <= 1){
+      if(! +month <= 1){
+        newMonth = +month -1
+        if(newMonth in thirtyone){
+          typeDay = "thirtyone"
+        }
+        else if(newMonth in thirty){
+          typeDay = "thirty"
+        }
+        else if(newMonth in twentyeight){
+          typeDay = "twentyeight"
+        }
+        var newDay = lastDayOfMonth.get(typeDay)
+      }
+      else{
+        newMonth = "NULL"
+        newDay = "NULL"
+        year = "NULL"
+      }
+    }else{
+      newDay = +day - 1
+      newMonth = month
+    }
+    var newDate = `${year}-${newMonth}-${newDay}`
+    return newDate
+  }
+
+  componentDidMount(){
+    const endpoint = 'https://api.nasa.gov/planetary/apod?'
+    const key = '2RameM4Tr39cfFBVk0hNhySsBeOONh1lEgK5rrp4'
+    fetch(`${endpoint}api_key=${key}&date=${this.getPreviousDate(this.state.oldDate)}`)
+      .then(res => res.json())
+      .then(res => this.setState({
+        newDate: res.date,
+        explanation: res.explanation,
+        mediaUrl: res.url,
+        mediaType: res.media_type,
+        isLoading: false
+      }))
+  }
+
+
+
+  render(){
+    return(
       <div className="previousApod">
-        <a href={props.mediaUrl} target="_blank" rel="noopener noreferrer">
-          {mediaType()}
+        <a href={this.state.mediaUrl} target="_blank" rel="noopener noreferrer">
+          <h2>Previous APOD</h2>
         </a>
-        <span>{props.date}</span>
+          {this.mediaType()} <br/>
+          <p>{this.state.explanation}</p>
+
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default PreviousApod
-
-
-// -----------------------------------------------------------------------------
-// import React from 'react'
-//
-// const PreviousApod = (props) => {
-//   if(props.date !== undefined){
-//     return (
-//       <div className="previousApod">
-//         <img src={props.mediaUrl} alt="prevApod"></img>
-//       </div>
-//     )
-//   }
-// }
-// export default PreviousApod
-
-// -----------------------------------------------------------------------------
-
-// class PreviousApod extends React.Component {
-//   constructor(){
-//     super()
-//     this.state = {
-//       total: undefined,
-//       date: undefined,
-//       explanation: undefined,
-//       mediaUrl: undefined,
-//       isLoading: true,
-//       resParam: undefined
-//     }
-//   }
-//
-//   splitDate = date => {
-//     console.log(date)
-//     var pino = date.split("-")
-//     return pino
-//   }
-//
-//   getPreviousDate = splittedDate => {
-//     var year = +splittedDate[0]
-//     var month = +splittedDate[1]
-//     var day = +splittedDate[2]
-//
-//     var thirtyone = [1,3,5,7,8,10,12]
-//     var thirty = [4,6,9,11]
-//     var twentyeight = [2]
-//
-//     const monthLenght = {
-//       "thirtyone" : "31",
-//       "thirty" : "30",
-//       "twentyeight" : "28"
-//     }
-//
-//     var previousDate = `${year}-${month}-${day-1}`
-//     console.log(previousDate)
-//     return previousDate
-//   }
-//
-//   apiCall(goodDate){
-//     const endpoint = 'https://api.nasa.gov/planetary/apod?'
-//     const key = '&api_key=2RameM4Tr39cfFBVk0hNhySsBeOONh1lEgK5rrp4&'
-//     const date = '&date=${goodDate}&'
-//     fetch(`${endpoint}${key}${date}`)
-//       .then(res => res.json())
-//       .then(res => this.setState({
-//         total: res,
-//         date: res.date,
-//         explanation: res.explanation,
-//         mediaUrl: res.hdurl,
-//         isLoading: false
-//       }))
-//   }
-//
-//   render(){
-//     if(this.state.date !== undefined){
-//       const splittedDate = this.splitDate(this.state.date)
-//       const prevDay = this.getPreviousDate(splittedDate)
-//       this.apiCall(prevDay)
-//       return (
-//         <div className="previousApod">
-//           <img src={this.state.mediaUrl} alt="prevApod"></img>
-//           <h3>{prevDay}</h3>
-//         </div>
-//       )
-//     }
-//
-//     else {
-//       return (
-//         <div className="previousApod">
-//           <img src={this.state.mediaUrl} alt="prevApod"></img>
-//         </div>
-//       )
-//     }
-//   }
-// }
