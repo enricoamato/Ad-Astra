c7dc79b8b57925712228e277c52c19ecbd4a9704
unknown <enricoamato997@gmail.com>
2019-03-02 15:59:47 +0100

    search has no its own component

---
+++ src/Search.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class Search extends React.Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = e => {
+    this.setState({ value: e.target.value })
+  }
+
+  search = e => {
+    e.preventDefault()
+    this.props.search(this.state.value)
+  }
+
+  render() {
+    return (
+      <div className="apodSearch">
+
+        <form onSubmit={this.search}>
+          <label>Search: </label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="YYYY-MM-DD"
+          />
+          <button type="submit">Search</button>
+        </form>
+
+      </div>
+    )
+  }
+}
+
+export default Search
