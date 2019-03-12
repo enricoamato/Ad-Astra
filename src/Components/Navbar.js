425a71359ab0fd6d9551b64d82c4e21125199134
unknown <enricoamato997@gmail.com>
2019-03-12 21:36:52 +0100

    modify links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,10 +4,9 @@ const Navbar = () => {
   return (
       <nav className="navbarContainer">
         <ul>
-          <li><a href="https://api.nasa.gov/">Home</a></li>
+          <li><a href="#">Home</a></li>
           <li><a href="https://api.nasa.gov/" target="_blank" rel="noopener noreferrer">NASA API</a></li>
-          <li><a href="https://api.nasa.gov/">About</a></li>
-          <li><a href="https://api.nasa.gov/">Resume</a></li>
+          <li><a href="./CV-ENG.pdf" download>Resume</a></li>
         </ul>
       </nav>
   )
