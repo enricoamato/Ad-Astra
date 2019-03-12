b37bab847e692f0d2a18d0ab3f2bc97276ba100d
unknown <enricoamato997@gmail.com>
2019-03-12 23:29:13 +0100

    fix react warning

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
       <nav className="navbarContainer">
         <ul>
-          <li><a href="#">Home</a></li>
+          <li><a href="./#">Home</a></li>
           <li><a href="https://api.nasa.gov/" target="_blank" rel="noopener noreferrer">NASA API</a></li>
           <li><a href="./CV-ENG.pdf" download>Resume</a></li>
         </ul>
