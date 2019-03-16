9717168cb506a5f07c60af380dba5956835831ba
unknown <enricoamato997@gmail.com>
2019-03-16 20:03:58 +0100

    add AppRouter and gallery styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Components/App.js'
+import AppRouter from './Components/AppRouter'
 import './assets/index.css'
+import './assets/gallery.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
